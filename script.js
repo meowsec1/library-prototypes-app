b37a5849c93e1cd4c08eaeadeacfd6d0aa7720e2
v <128931588+meowsec1@users.noreply.github.com>
2025-08-05 16:29:12 -0400

    added book removing capabilities

--- script.js
+++ script.js
@@ -43,14 +43,27 @@ function displayBooks(books) {
             bookRead.innerHTML = 'Not yet read!';
         }
 
+
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(bookPages);
         bookCard.appendChild(bookRead);
+        bookCard.setAttribute("data-index-number", book.id);
+
+        bookCard.innerHTML += `<svg class="remove" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>`;
+
+        const removeBtn = bookCard.querySelector(".remove");
+        removeBtn.addEventListener("click", function(){
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].id == book.id) {
+                    myLibrary.splice(i)
+                    displayBooks(myLibrary)
+                }
+            }
+        })
 
         bookContainer.appendChild(bookCard);
     }
-
 }
 
 
